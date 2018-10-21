0617448bbb6f90e47f6198daaf5fdeea7c0be36b
Anatolii <anatolii.kryvda@gmail.com>
2018-10-22 02:26:49 +0300

    remove bank related files to make project dedicated to sudoku only

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 import Game from "./Game";
-import AppBanks from "./AppBanks";
-import AppBankList from "./AppBankList";
-import AppBankGovList from "./AppBankGovList";
 
-ReactDOM.render(<AppBankGovList />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
